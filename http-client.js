1cb01d0896bf20f180c03d21ac3ba3abd542c28e
tarekbellalouna <tarek.bellalouna@gmail.com>
2020-02-07 12:50:20 +0100

    first commit

---
+++ http-client.js
@@ -0,0 +1,14 @@
+var http = require('http');
+ 
+http.get(process.argv[2], function (response) {
+    response.setEncoding('utf8');
+    response.on('data', console.log);
+    response.on('error', console.error);
+});
+var http = require('http');
+ 
+http.get(process.argv[2], function (response) {
+    response.setEncoding('utf8');
+    
+});
+
